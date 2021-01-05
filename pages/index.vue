733f4b0248d236a43021cd75f6d29b7b0c52ab04
Jon Webb <jon@jonwebb.dev>
2021-01-05 15:00:13 -0500

    Framework for index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,69 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div id="navigation">
+      <div class="flex">
+        <img src="~/assets/images/logo.png" alt="Stroup Styles logo" />
+        <a href="" class="italic">About Me</a>
+        <a href="" class="italic">See Styles</a>
+        <a href="" class="italic">Get Connected</a>
+      </div>
+    </div>
+    <div id="cta">
+      <div id="cta-main">
+        <h1>Simply beautiful</h1>
+        <p>
+          My name is Kathryn Stroup, and my passion is helping others see the
+          beauty I see, making their dreams a reality, and being a part of the
+          excitement in the small in-betweens. I'd love to get to know you and
+          your story. If you have any questions just message me below:
+        </p>
+        <form action="submit">
+          <input type="email" placeholder="Email" />
+          <input type="textarea" placeholder="Tell me about yourself..." />
+          <input type="submit" value="Get connected!" />
+        </form>
+      </div>
+      <div id="cta-images"></div>
+    </div>
+    <div id="about">
+      <h1>Behind the Chair</h1>
+      <p>
+        I have always found joy in caring for others; in the little daily
+        things, making a meal for friends, or simply talking about life. Bridal
+        hair has become a passion of mine because I love to care for and be a
+        sense of calm for brides on one of the biggest days of their life. The
+        quiet moments leading up to their wedding, hearing about their love
+        story, and making them feel their most beautiful before marrying the
+        love of their life is why I love what I do!
+      </p>
+    </div>
+    <div id="testimonials">
+      <div id="testimonials-images"></div>
+      <div id="testimonials-main">
+        <h2>brides</h2>
+        <h1>Abigail Krause</h1>
+        <p>
+          "Thank <b class="uppercase">you</b> so much for everything you did to
+          help make the day perfect!"
+        </p>
+        <a href="">See more styles →</a>
+      </div>
+    </div>
+    <div id="contact">
+      <h1>Let't Talk</h1>
+      <form action="submit">
+        <input type="email" placeholder="Email" />
+        <input type="textarea" placeholder="Tell me about yourself ..." />
+        <input type="submit" value="Get connected!" />
+      </form>
+    </div>
+    <div id="instagram">
+      <h1>Shop the Look</h1>
+      <h2>on instagram</h2>
+      <div id="instagram-feed"></div>
+    </div>
+    <div id="footer"><p>© StroupStyles 2020.</p></div>
+  </div>
 </template>
 
 <script>
