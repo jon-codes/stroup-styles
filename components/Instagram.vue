a8c7fb7ac92e492e6d16dfb4fd1b60dd462ee2ee
Jon Webb <jon@jonwebb.dev>
2021-01-06 14:08:00 -0500

    Add new widget

--- components/Instagram.vue
+++ components/Instagram.vue
@@ -5,14 +5,18 @@
     <div class="bg-light xl:mb-20 w-48 h-6 mx-auto mb-8"></div>
     <div id="instagram-feed">
       <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+
+      <!-- Desktop -->
+      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
       <iframe
-        src="//lightwidget.com/widgets/32507e45a588582298710cc0f836a327.html"
+        src="https://cdn.lightwidget.com/widgets/32507e45a588582298710cc0f836a327.html"
         scrolling="no"
         allowtransparency="true"
         class="lightwidget-widget xl:block hidden"
         style="width: 100%; border: 0; overflow: hidden"
       ></iframe>
 
+      <!-- Mobile -->
       <iframe
         src="//lightwidget.com/widgets/e490eb077ecb515192601db81f0d80a7.html"
         scrolling="no"
